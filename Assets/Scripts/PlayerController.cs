0d1b325703d3fa90ad6e1f0e8779a1c7ab2e24c4
aminbnd <2212@holbertonschool.com>
2021-08-20 16:46:41 +0200

    Task 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,11 @@ public class PlayerController : MonoBehaviour
             health--;
             Debug.Log($"Health: {health}");
         }
+
+        if(other.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
     }
     
     
